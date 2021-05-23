934ec11820e006af780b196947467d400d11b48e
oscarcole <33353992+oscarcole@users.noreply.github.com>
2021-05-22 18:56:27 -0600

    Initial commit

---
+++ pom.py
@@ -0,0 +1,15 @@
+"""
+Requirements
+
+1. Go to www.makemytrip.com
+2. Click on flights link and click on round trip
+3. Select from: Delihi and To: Bangalore
+4. Select departure date (today's date) and return date: after 7 days
+5. Click on search
+6. Print total number of records of 'Departure Flight' and 'Return Flight' on the console
+7. Select Non-Stop and 1 Stop filter options and print total number of records of 'Departure Flight, and 'Return flight'
+on the console
+8. Select radio button on top 10 options of 'Departure Flight' and 'Return Flight'
+9. Verify the same Departure Flight price and Return Flight price are getting reflected at the bottom of the page
+10. Verify the correct total amount (Departure Flight price + Return Flight Price) is getting reflected correctly
+"""
